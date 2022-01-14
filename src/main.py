e298bcd5d18f826424afbc1a55ee9bac7124a742
酒井裕行 <zyoushu36@gmail.com>
2022-01-14 16:52:58 +0900

    modified files

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+
 from torch import nn
 from torch.optim import Adam, SGD
 
@@ -9,24 +11,31 @@ from experiment import experiment
 
 
 if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--csv_dir', type=str, default='results/csv/efficientnet-b0')
+    parser.add_argument('--prm_dir', type=str, default='results/prm/efficientnet-b0')
+    args = parser.parse_args()
+    csv_dir = args.csv_dir
+    prm_dir = args.prm_dir
+
     opt_dict = {
         'adam': Adam,
         'sgd': SGD,
         'adabelief': AdaBelief
     }
 
-    for opt_name, opt_type in opt_dict:
+    for opt_name in opt_dict:
         model = EfficientNet.from_pretrained('efficientnet-b0')
         model._fc = nn.Linear(model._fc.in_features, 10)
-        optimizer = opt_type(model.parameters(), lr=1e-3, betas=(0.9, 0.999))
+        optimizer = opt_dict[opt_name](model.parameters(), lr=1e-3, betas=(0.9, 0.999))
         summary(model, [3, 32, 32])
 
         experiment(
             model = model,
             optimizer = optimizer,
             max_epoch = 200,
-            csv_dir = 'results/csv/efficientnet-b0',
+            csv_dir = csv_dir,
             csv_name = f'{opt_name}.csv',
-            prm_dir = 'results/prm/efficientnet-b0',
+            prm_dir = prm_dir,
             prm_name = f'{opt_name}.prm',
         )
