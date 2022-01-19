7ad1277cfde08b1c54a993ea1ed1c7c04e71cbb1
酒井裕行 <zyoushu36@gmail.com>
2022-01-19 15:55:47 +0900

    modify files

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python src/main.py --csv_dir 'results/csv' \
-                   --pt_dir 'results/pt' \
+                   --pth_dir 'results/pt' \
                    --max_epoch 100 \
                    --batch_size 256 \
                    --cutout \
