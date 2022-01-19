71ff58304b766a5f12c873ce69c28e5009a8e90c
酒井裕行 <zyoushu36@gmail.com>
2022-01-19 18:38:32 +0900

    plot

--- src/cm.py
+++ src/cm.py
@@ -11,13 +11,13 @@ from torchvision.transforms import ToTensor
 from efficientnet_pytorch import EfficientNet
 
 
-if __name__ == '__main__':
+def cm(path):
     model = EfficientNet.from_name('efficientnet-b0')
     model._fc = nn.Linear(model._fc.in_features, 10)
-    model.load_state_dict(torch.load('results/pth/efficientnet-b0/adam+cutout.pth'))
+    model.load_state_dict(torch.load(path))
 
     test_data = CIFAR10('./data', train=False, download=False, transform=ToTensor())
-    test_loader = DataLoader(test_data, batch_size=256, shuffle=False)
+    test_loader = DataLoader(test_data, batch_size=100, shuffle=False)
 
     y_true = []
     y_pred = []
@@ -28,6 +28,8 @@ if __name__ == '__main__':
         y_true += labels.to('cpu').detach().numpy().tolist()
         y_pred += predicted.to('cpu').detach().numpy().tolist()
 
+    return confusion_matrix(y_true, y_pred)
 
-    cm = confusion_matrix(y_true, y_pred)
-    print(cm)
+
+if __name__ == '__main__':
+    print(cm('results/pth/efficientnet-b0/adam+cutout.pth'))
