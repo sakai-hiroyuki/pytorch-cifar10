46a83c04d9052a277652e4cb95583a0b8feb1f8f
酒井裕行 <zyoushu36@gmail.com>
2022-01-22 00:37:10 +0900

    modify files

--- src/main.py
+++ src/main.py
@@ -84,7 +84,7 @@ class ExperimentCIFAR10:
         if not os.path.isdir(self.pth_dir):
             os.makedirs(self.pth_dir)
 
-        loaders = self.prepare_data(self.data_dir, batch_size=self.batch_size, cutout=self.cutout)
+        loaders = self.prepare_data()
         train_loader = loaders[0]
         test_loader = loaders[1]
 
@@ -172,25 +172,32 @@ if __name__ == '__main__':
     parser.add_argument('-e', '--max_epoch', type=int, default=100)
     parser.add_argument('-b', '--batch_size', type=int, default=1024)
     parser.add_argument('-o', '--optimizer', type=str, default='momentum')
+    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
+    parser.add_argument('-wd', '--weight_decay', type=float, default=0.0)
     parser.add_argument('--cutout', action='store_true')
     parser.add_argument('-csv', '--csv_dir', type=str, default='results/csv')
     parser.add_argument('-pth', '--pth_dir', type=str, default='results/pth')
     args = parser.parse_args()
 
-    opt_dict = {
-        'sgd': (SGD, {'lr': 1e-3}),
-        'momentum': (SGD, {'lr': 1e-3, 'momentum': 0.9}),
-        'rmsprop': (RMSprop, {'lr': 1e-3}),
-        'adam': (Adam, {'lr': 1e-3}),
-        'amsgrad': (Adam, {'lr': 1e-3, 'amsgrad': True}),
-        'adabelief': (AdaBelief, {'lr': 1e-3}),
+    optimizer_dict = {
+        'sgd': (SGD, {}),
+        'momentum': (SGD, {'momentum': 0.9}),
+        'rmsprop': (RMSprop, {}),
+        'adam': (Adam, {}),
+        'amsgrad': (Adam, {'amsgrad': True}),
+        'adabelief': (AdaBelief, {}),
     }
 
     model = EfficientNet.from_pretrained('efficientnet-b0')
     model._fc = nn.Linear(model._fc.in_features, 10)
     summary(model, [3, 32, 32])
 
-    optimizer = opt_dict[args.optimizer][0](model.parameters(), **opt_dict[args.optimizer][1])
+    optimizer = optimizer_dict[args.optimizer][0](
+        model.parameters(),
+        lr = args.learning_rate,
+        weight_decay = args.weight_decay,
+        **optimizer_dict[args.optimizer][1]
+    )
     print(optimizer)
 
     scheduler = None
@@ -214,6 +221,7 @@ if __name__ == '__main__':
         csv_name = csv_name,
         pth_dir = args.pth_dir,
         pth_name = pth_name,
+        use_tqdm=True,
     )
 
     experiment()
