d85d19404f1f8210c4612dc26f303c70179dff89
酒井裕行 <zyoushu36@gmail.com>
2022-01-25 16:40:11 +0900

    lr_scheduler

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 python src/main.py -o 'momentum' \
                     -m 'resnet20-cifar10' \
-                    -e 100 \
+                    -s 1 \
+                    -e 300 \
                     -b 1024 \
+                    -lr 0.1 \
                     -csv 'results/csv' \
                     -pth 'results/pth' \
                     --weight_decay 1e-4
